baf31b000a954c80dafb4a89bc31dd3338039907
pdeepak020 <138395421+pdeepak020@users.noreply.github.com>
2026-03-25 11:03:09 +0530

    Delete calculator_django directory

--- calculator_django/calc_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.calculator, name='calculator'),
-    path('calculate/', views.calculate, name='calculate'),
-] 
